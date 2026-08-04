9449e1c51877f1646013f73738c41aaafe5388a3
katta sathvik <f20200191@hyderabad.bits-pilani.ac.in>
2026-08-04 18:14:15 +0530

    added some generic comments for previous commit before this

--- src/components/posts/editor/mutations.ts
+++ src/components/posts/editor/mutations.ts
@@ -13,7 +13,7 @@ export function useSubmitPostMutation(){
 
     const mutation = useMutation({
       // mutationFn: submitPost, 
-      //instead of simply submitPost we're writing a manual function which calls submitPost inside it and returns the value. We're doign this because in production nextjs deliberately strips any error messages thrown inside the server action, replacing it with that generic "error occurred in Server Components render" text + a digest. In our app when user types any profane word and tries to submit, then in development a proper error toast message is shown, but in production a generic error message is displayed as shown above. So to fix this, The fix: don't throw from the Server Action for expected, user-facing errors — return a result object instead, and throw on the client side, since client-thrown errors are never touched by this sanitization (it only applies to code executing in the server action context), in the actions.ts file we already wrapped the await moderateText(content) in a try/catch so it throws error, and it returns an object response with success field and err field. and that we will handle that error here in the client side.
+      //instead of simply submitPost we're writing a manual function which calls submitPost inside it and returns the value. We're doign this because in production nextjs deliberately strips any error messages thrown inside the server action, replacing it with that generic "error occurred in Server Components render" text + a digest(I wrote full error below at last). In our app when user types any profane word and tries to submit, then in development a proper error toast message is shown("Content contains disallowed profanity. Please remove explicit language and try again."), but in production a generic error message is displayed as shown above. So to fix this, The fix: don't throw from the Server Action for expected, user-facing errors return a result object instead, and throw on the client side, since client-thrown errors are never touched by this sanitization (it only applies to code executing in the server action context), in the actions.ts file we already wrapped the await moderateText(content) in a try/catch so it throws error, and it returns an object response with success field and err field. and that we will handle that error here in the client side.
 
       mutationFn: async (input: Parameters<typeof submitPost>[0]) => {
         const result = await submitPost(input);
@@ -105,3 +105,9 @@ export function useSubmitPostMutation(){
 
     return mutation;
 }
+
+/*
+Generic error message in case of server action error in production
+"An error occurred in the Server Components render. The specific message is omitted in production builds to avoid leaking sensitive details. A digest property is included on this error instance which may provide additional details about the nature of the error."
+
+*/
